a1e6af12f291d6a7a4d06265e8546c9d5ba1f90e
Fantox <shubhapratimbiswas@gmail.com>
2022-05-26 22:27:50 +0530

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -16,7 +16,7 @@ bot(
 	async (message, match) => {
 		if (!global.AFK.isAfk && !match)
 			return await message.sendMessage(
-				'Example : My owner is AFK\n last seen before #lastseen\nTo off afk send a msg again.'
+				'Example : My owner is Offline\n last seen before #lastseen\nTo turn off afk send a msg again.'
 			)
 		if (!global.AFK.isAfk) {
 			if (match) global.AFK.reason = match
